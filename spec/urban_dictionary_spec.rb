8c4fc4f43b36f45a608575530d2eab7b9ace7f58
Sebastian Adam Prokuski <Subconscious@Sebastians-iMac.local>
2016-07-10 19:38:38 -0700

    add second passing rspec test for words.rb

--- spec/urban_dictionary_spec.rb
+++ spec/urban_dictionary_spec.rb
@@ -6,10 +6,16 @@ describe(Word) do
     Word.clear()
   end
 
-  decsribe('#initialize') do
+  describe('#initialize') do
     it('returns the word for a given word object') do
       test_word = Word.new({:word => "test"})
       expect(test_word.word()).to eq("test")
+    end
+  end
+
+  describe('.all') do
+    it('returns an empty array before first word submission') do
+      expect(Word.all()).to eq([])
     end
   end
 end
