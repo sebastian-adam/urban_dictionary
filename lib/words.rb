075c83a4d4798f2d5a7a4bfc9ac32196983c5b1a
Sebastian Adam Prokuski <Subconscious@Sebastians-iMac.local>
2016-07-10 19:34:57 -0700

    add first passing rspec test for words.rb

--- lib/words.rb
+++ lib/words.rb
@@ -7,7 +7,11 @@ class Word
     @word = attributes.fetch(:word)
   end
 
-  define_method_singleton_method(:clear) do
+  define_singleton_method(:clear) do
     @@words = []
+  end
+
+  define_singleton_method(:all) do
+    @@words
   end
 end
